3440c76d82befa19900fa4532dd08e16654517c7
Vishal Singh <31471251+vsingh16@users.noreply.github.com>
2021-02-12 09:33:23 +0530

    Update LinkedList.java

--- src/linklist/LinkedList.java
+++ src/linklist/LinkedList.java
@@ -51,5 +51,57 @@ Class LinkedList{
         return head;
     }
     
+ /**
+ Deletes xth node.
+ Time Complexity: O(n)
+ **/
+  void deleteNode(Node head, int x)
+    {
+	   int counter = 1;
+	   
+	   //if head element is to be deleted
+	   if(head!=null && counter == x){
+	       head = head.next;
+	       return head;
+	   }
+	   
+	   Node previous = head;
+	   while(previous!=null && previous.next!=null && counter+1 != x){ // counter + 1 : denotes previous node
+	       previous = previous.next;
+	       counter++;
+	   }
+	   
+	   if(counter+1 == x){
+	       previous.next =  previous.next!=null ? previous.next.next : null;
+	   }
+	   
+	   return head;
+    }
+  
+   /**
+    Deletes node with value x.
+    Time Complexity: O(n)
+   **/
+  void deleteNode(Node head, int x)
+    {
+	   	   
+	   //if head element is to be deleted
+	   if(head!=null && head.data == x){
+	       head = head.next;
+	       return head;
+	   }
+	   
+	   Node previous = head;
+	   while(previous!=null && previous.next!=null && previous.next.data != x){ 
+	       previous = previous.next;
+	       counter++;
+	   }
+	   
+	   if(previous.next.data == x){
+	       previous.next =  previous.next!=null ? previous.next.next : null;
+	   }
+	   
+	   return head;
+    }
     
-  }
+ }
