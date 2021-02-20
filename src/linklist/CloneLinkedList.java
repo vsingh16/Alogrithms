b24b222e519bee2db39fbc200c9c1a623d63a9b5
Vishal Singh <31471251+vsingh16@users.noreply.github.com>
2021-02-20 19:33:32 +0530

    Update CloneLinkedList.java

--- src/linklist/CloneLinkedList.java
+++ src/linklist/CloneLinkedList.java
@@ -1,6 +1,7 @@
 /**
 https://www.youtube.com/watch?v=VNf6VynfpdM
 https://www.geeksforgeeks.org/clone-linked-list-next-random-pointer-o1-space/
+https://leetcode.com/problems/copy-list-with-random-pointer/submissions/
 
 Clone a linked list with next and random pointer in O(1) space
 Given a linked list having two pointers in each node. The first one points to the next node of the list, however, the other pointer is random and can point to any node of the list. Write a program that clones the given list in O(1) space, i.e., without any extra space.
