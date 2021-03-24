bcd949162bd6fd39a6024b8795b79d94eb3e064e
Vishal Singh <31471251+vsingh16@users.noreply.github.com>
2021-03-24 09:44:35 +0530

    Update LeftViewBinaryTree.java

--- src/tree/LeftViewBinaryTree.java
+++ src/tree/LeftViewBinaryTree.java
@@ -7,10 +7,14 @@
 **          6   7
 ** Left View = at each level the very first node i.e 4, 5,3,6
 **
-** Approach : Do pre order traversal. Have a global variable which wil be initially o , maxLevel < level.
+** Approach1 : Do pre order traversal. Have a global variable which wil be initially o , maxLevel < level.
 ** Only for very first node this maxLevel < level will be true as for later ones it will be height of tree., hence false
 ** Time Complexity : O(n)
 ** Space Complexity : O(h), recursive calls
+
+** Approach 2: Do level order traversal and print element when i == 0
+** Time Complexity : O(n)
+** Space Complexity : O(n)
 **/
 
 class Tree
@@ -39,6 +43,42 @@ class Tree
         
         leftViewUtil(root.left, level + 1);
         leftViewUtil(root.right, level + 1);
+   }
+    
+   //Approach 2: 
+   ArrayList<Integer> leftView2(Node root)
+    {
+    
+      ArrayList<Integer> result = new ArrayList();
+      Queue<Node> queue = new LinkedList();
+        
+        if(root !=null){
+            queue.add(root);
+        }
+        
+        
+        while(!queue.isEmpty()){
+            //System.out.println("Size:" +queue.size());
+            int children = queue.size(); // PS: loop will run as per children in a level
+            for(int i= 0;i < children; i++){                
+                Node node = queue.poll();
+                if(i == 0){
+                    result.add(node.data); // add first child element only
+                }
+                if(node.left != null){
+                    queue.add(node.left);    
+                }
+                if(node.right != null){
+                    queue.add(node.right);    
+                }                                
+            }
+        }
+        
+        return result;
     }
+     
+    
+    
+    
 }
 
