6d1477c2f0fc25e0b34e3bd8a5b5c27832403a4c
Vishal Singh <31471251+vsingh16@users.noreply.github.com>
2021-07-01 09:13:55 +0530

    Create IsConnectedGraph.java

---
+++ src/graph/IsConnectedGraph.java
@@ -0,0 +1,70 @@
+/**
+** https://thecodingsimplified.com/check-if-directed-graph-is-strongly-connected/
+**/
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+class Graph {
+  Map<List<Integer>> graph;  
+  int nodes;
+
+  Graph(int nodes) {
+    graph = new HashMap<>();    
+    this.nodes = nodes;
+
+    for (int i = 0; i < nodes; i++) {
+      graph.get(i, new ArrayList<>());
+    }
+  }
+
+  public void addEdge(int a, int b) {
+    graph.get(a).add(b);
+  }
+  
+  public boolean ifDirectedGraphStronglyConnected() {
+
+    for(int i = 0; i < nodes; i++) {
+      dfs(i);  
+      boolean visited [] = new boolean[nodes];      
+      for(int j = 0; j < nodes; j++) {
+        if(!visited[j]) {
+          return false;
+        }
+      }            
+    }
+    
+    return true;
+  }
+
+  public void dfs(int start) {
+    visited[start] = true;
+
+    List<Integer> neighboursList = graph.get(start);
+
+    for (Integer neighbour : neighboursList) {
+      if (!visited[neighbour]) {
+        dfs(neighbour);
+      }
+    }
+  }
+}
+
+public class GraphProblems {
+
+  public static void main(String[] args) {
+    int nodes = 4;
+    
+    Graph a = new Graph(nodes);
+    
+    a.addEdge(0, 1);
+    a.addEdge(1, 2);
+    a.addEdge(2, 0);
+    a.addEdge(0, 3);
+    a.addEdge(3, 2);
+
+    System.out.println(a.ifDirectedGraphStronglyConnected());
+  }
+
+}
