ab7b3b63f030bde8bd6c12ca78e084bcb1b83897
Vishal Singh <31471251+vsingh16@users.noreply.github.com>
2021-07-11 11:16:19 +0530

    Create FindMinAndMax.java

---
+++ src/array/FindMinAndMax.java
@@ -0,0 +1,9 @@
+/**
+** Find min and max in an array
+** eg : {10,4,1,2,80}.
+** min = 1, max = 80
+** Appraoch 1: Sort, min = a[0] and max = a[n-1] , Time Complexity : O(nlogn)
+** Approach 2: Min = a[0], Max = a[0], iterate afterwards in array, if a[i] < min, min = a[i]
+** a[i] > max, max = a[i]
+** Time Complexity: O(n)
+**/
