a5c640f0293d4e367d67b0934de39ea89c94e2af
Vishal Singh <31471251+vsingh16@users.noreply.github.com>
2021-07-24 14:48:58 +0530

    Update LongestPalindromeString.java

--- src/dynamic/programming/LongestPalindromeString.java
+++ src/dynamic/programming/LongestPalindromeString.java
@@ -4,44 +4,61 @@ package dynamic.programming;
  * Created by vishal on 29-Apr-18.
  * <p>
  * https://www.geeksforgeeks.org/longest-palindrome-substring-set-1/
+ * https://leetcode.com/problems/longest-palindromic-substring/submissions/
  * <p>
  * Approach1:
  * find all substring , then find longest len palindrome among it.
  * Time Complexity:O(n^3)
  * <p>
  * Approach2:
+ * Since this is substring, it has to be continuous,
+ * Case 1: if String is of length = 1, i.e i == j =>  dp[i][j] =1
+ * Case 2: if len is 2, i+1 ==j , str[i] == str[j], dp[i][j] = 2 
+ * Case 3: if len > 2 and str[i] == str[j] & str[i+1] == str[j-1], dp[i][j] = 2+dp[i+1][j-1] else 0.
+ * We will keep max variable
  * Using DP:palindomre(i,j) = if(str[i] == str[j]) and substring(i+1,j-1) is palindrome
  * then substring(i,j) is also palindorme
  */
 public class LongestPalindromeString {
 
-    public static int longestPalindromeSubString(String s) {
-
-        int palindrome[][] = new int[s.length()][s.length()];
-        int max = Integer.MIN_VALUE;
-
-        for (int i = palindrome.length - 1; i >= 0; i--) {
-            for (int j = i; j < palindrome[0].length; j++) {
+    // Time Complexity: O(n^2)
+    //Space Complexity: O(n^2)
+      public String longestPalindrome(String str) {
+        int n = str.length();
+        int dp[][] = new int[n][n];        
+        int max=-1;        
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j < n; j++) {
+                //one length
                 if (i == j) {
-                    palindrome[i][i] = 1;
-                } else {
-                    if (s.charAt(i) == s.charAt(j) && palindrome[i + 1][j - 1] > 0) {
-                        palindrome[i][j] = palindrome[i + 1][j - 1] + 2;
-                    } else {
-                        palindrome[i][j] = 0;
+                    dp[i][j] = 1;                    
+                    max = Math.max(max, dp[i][j]);                    
+                } else if (j > i) { // more than 1 length
+                    if (str.charAt(i) == str.charAt(j) && i+1 == j) {//len = 2
+                        dp[i][j] = 2;
+                        max = Math.max(max, dp[i][j]);
+                    } else if(str.charAt(i) == str.charAt(j) && dp[i+1][j-1] > 0){ //len >2
+                        dp[i][j] = 2+ dp[i+1][j-1];
+                        max = Math.max(max, dp[i][j]);
+                    }else{
+                        dp[i][j] = 0;
                     }
                 }
-                max = Math.max(palindrome[i][j], max);
             }
         }
-
-        return max;
-
-    }
-
-    public static void main(String[] args) {
-        System.out.println(longestPalindromeSubString("TALAT"));//5
-        System.out.println(longestPalindromeSubString("TAL"));//0
-        System.out.println(longestPalindromeSubString("VTALATI"));//5
+                
+        int posi=-1,posj=-1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+     
+                if(dp[i][j] == max){                    
+                    posi=i;
+                    posj=j;
+                }
+            }
+     
+        }
+        
+        return str.substring(posi,posj+1);
     }
 }
