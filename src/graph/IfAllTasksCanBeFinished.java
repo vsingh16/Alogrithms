b1428049793b8e6a3ce11deadddb82c792c7a45c
Vishal Singh <31471251+vsingh16@users.noreply.github.com>
2021-09-09 10:04:57 +0530

    Create IfAllTasksCanBeFinished.java

---
+++ src/graph/IfAllTasksCanBeFinished.java
@@ -0,0 +1,18 @@
+Ref Link : https://www.youtube.com/watch?v=mBLKHhdFdcw&list=PLDdcY4olLQk066ysRibhoGd3UzGr0XSQG&index=15
+https://www.geeksforgeeks.org/find-whether-it-is-possible-to-finish-all-tasks-or-not-from-given-dependencies/
+
+Approach: we can form graph for given tasks.
+  Apply Algos like DFS(order[]) and topological sort to check if there is a cycle in Directed graph.
+  If cycle tasks can't be finished else possible.
+
+
+
+
+
+
+
+
+
+
+
+
