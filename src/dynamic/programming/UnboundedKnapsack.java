eb17284850db41d3c96e2d0e3675f7924d32635a
Vishal Singh <31471251+vsingh16@users.noreply.github.com>
2021-09-12 10:55:53 +0530

    Update UnboundedKnapsack.java

--- src/dynamic/programming/UnboundedKnapsack.java
+++ src/dynamic/programming/UnboundedKnapsack.java
@@ -1,6 +1,7 @@
 /**
 ** https://www.geeksforgeeks.org/unbounded-knapsack-repetition-items-allowed/
-** Given a knapsack weight W and a set of n items with certain value vali and weight wti, we need to calculate the maximum amount that could make up this quantity exactly. This is different from classical Knapsack problem, here we are allowed to use unlimited number of instances of an item.
+** Given a knapsack weight W and a set of n items with certain value vali and weight wti, we need to calculate the maximum amount that could make up 
+** this quantity exactly. This is different from classical Knapsack problem, here we are allowed to use unlimited number of instances of an item.
 Examples: 
 
 Input : W = 100
