c12649fdde70cdfbff7c417e4f6b78587ebe6126
Vishal Singh <31471251+vsingh16@users.noreply.github.com>
2021-09-25 11:16:15 +0530

    Update WildcardPatternMatching.java

--- src/dynamic/programming/WildcardPatternMatching.java
+++ src/dynamic/programming/WildcardPatternMatching.java
@@ -1,6 +1,7 @@
 /**
 ** https://www.geeksforgeeks.org/wildcard-pattern-matching/
-** Given a text and a wildcard pattern, implement wildcard pattern matching algorithm that finds if wildcard pattern is matched with text. The matching should cover the entire text (not partial text).
+** Given a text and a wildcard pattern, implement wildcard pattern matching algorithm that finds if wildcard pattern is matched with text.
+** The matching should cover the entire text (not partial text).
 The wildcard pattern can include the characters ‘?’ and ‘*’ 
 ‘?’ – matches any single character 
 ‘*’ – Matches any sequence of characters (including the empty sequence)
