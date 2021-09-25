2a60796839e5820a7cf583e448339b8e1480f542
Vishal Singh <31471251+vsingh16@users.noreply.github.com>
2021-09-25 11:23:39 +0530

    Update CountAllPossiblePathsInMatrix.java

--- src/dynamic/programming/CountAllPossiblePathsInMatrix.java
+++ src/dynamic/programming/CountAllPossiblePathsInMatrix.java
@@ -3,7 +3,8 @@
 ** Count all possible paths from top left to bottom right of a mXn matrix
 Difficulty Level : Easy
 Last Updated : 24 Aug, 2021
-The problem is to count all the possible paths from top left to bottom right of a mXn matrix with the constraints that from each cell you can either move only to right or down
+The problem is to count all the possible paths from top left to bottom right of a mXn matrix with the constraints that from each cell you can 
+either move only to right or down
 Examples : 
  
 
