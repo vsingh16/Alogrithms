a0632940263d300954e7990b2700e91d37a2021b
Vishal Singh <31471251+vsingh16@users.noreply.github.com>
2021-09-27 10:45:08 +0530

    Update Stairs.java

--- src/dynamic/programming/Stairs.java
+++ src/dynamic/programming/Stairs.java
@@ -3,6 +3,8 @@ package dynamic.programming;
 /**
  * There are n stairs, a person standing at the bottom wants to reach the top. 
  * The person can climb either 1 stair or 2 stairs at a time. Count the number of ways, the person can reach the top.
+ ** https://www.geeksforgeeks.org/count-ways-reach-nth-stair/
+ ** https://www.techiedelight.com/find-total-ways-reach-nth-stair-with-atmost-m-steps/
  */
 public class Stairs {
     
