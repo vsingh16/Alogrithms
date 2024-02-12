f18f665e64bbce95be030c0e38b75387f1b71354
Vishal Singh <31471251+vsingh16@users.noreply.github.com>
2024-02-12 07:14:40 +0000

    Update FindAPairWithGivenDifference.java

--- src/array/Search/FindAPairWithGivenDifference.java
+++ src/array/Search/FindAPairWithGivenDifference.java
@@ -1,4 +1,5 @@
 /**
+** https://www.geeksforgeeks.org/find-a-pair-with-the-given-difference/
 ** Find a pair with the given difference
 ** Given an unsorted array and a number n, find if there exists a pair of elements in the array whose difference is n. 
 ** Examples: 
@@ -18,9 +19,31 @@
 ** i=0, j=1
 ** find a[j]-a[i]
 * Case 1: a[j]-a[i] == k -> return 
-** Case 2: a[j]-a[i] < k -> j++. Since distance is less than k and array is sorted, we need to increase value of j
-** Case 3: a[j]-a[i] > k -> i++. Since distance is greater than k and array is sorted, we need to increase value of i
+** Case 2: a[j]-a[i] < k -> j++. Since the distance is less than k and the array is sorted, we need to increase the value of j
+** Case 3: a[j]-a[i] > k -> i++. Since the distance is greater than k and the array is sorted, we need to increase the value of i
 ** Time Complexity: O(nlogn). Space Complexity: O(1)
 ** Ref: https://www.youtube.com/watch?v=XGrXiVi7Ces&t=154s
 ** 
 **/
+
+
+class Solution
+{
+    public boolean findPair(int a[], int size, int k)
+    {
+       
+       Arrays.sort(a);
+       int i=0,j=1;
+       while(i< size && j < size){
+            if(i!=j && ((a[j] - a[i]) == k) ){ //i!=j i.e distinct elements. This will take care of case where K =0
+                return true;
+            }else if((a[j] - a[i]) < k){
+                j++;
+            }else{
+                i++;
+            }           
+       }
+       
+       return false;
+    }
+}
