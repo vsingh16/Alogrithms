6e3682fbf6e0c60ed29cb37ef76699be40ab6504
Vishal Singh <31471251+vsingh16@users.noreply.github.com>
2024-08-31 10:10:09 +0100

    Update TrieLongestValidWordWithAllPrefixes.java

--- TrieLongestValidWordWithAllPrefixes.java
+++ TrieLongestValidWordWithAllPrefixes.java
@@ -45,7 +45,7 @@ class Solution {
     ** Time Complexity: for each word, we are doing a depth-first search in Trie.
     ** Trie Height will be the length of the max word.
     ** O(n * L), n = no of wrods in array. L = Length of max word.
-    
+    ** Space Complexity: O(26 * N * L)
     **/
      public static void longestString(TrieNode root, StringBuilder temp) {
 
