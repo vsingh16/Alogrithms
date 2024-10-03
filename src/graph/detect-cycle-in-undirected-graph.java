3d7a77d6a7e220b91158adf87e7eb203d6cbfe5c
Vishal Singh <31471251+vsingh16@users.noreply.github.com>
2024-10-03 16:47:08 +0100

    Update detect-cycle-in-undirected-graph.java

--- src/graph/detect-cycle-in-undirected-graph.java
+++ src/graph/detect-cycle-in-undirected-graph.java
@@ -1,10 +1,14 @@
 /**
 ** https://www.geeksforgeeks.org/detect-cycle-undirected-graph/
 ** https://practice.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1
+** Ref: https://www.youtube.com/watch?v=59fUtYYz7ZU&t=9169s. Apna College Graph
 ** Approach :
 ** Apply DFS and see if a node is already visited.
-** Since undirected graph a node can be visted twice eg 1 -- 2 , when we visit 1, visited[1] = true and then again we can visite 1 from 2
-** if a node is alreayd visited but this visit is not from its parent node i.e for a vertex V, negihbour of V ! = parent, then we can say its cycle.
+** Since undirected graph a node can be visted twice eg 1 -- 2 , when we visit 1, visited[1] = true and then again we can visit 1 from 2
+** That means while exploring neigbours, we need to skip a neighbour if it is same as parent, eg in this case from 2 to 1 , parent is 1 and neighbour for 2 is also 1.
+** We can skip it.
+** Else 
+** Simply check is neighbour is already visited true, then it is a cycle.
 ** https://www.youtube.com/watch?v=6PSczvPWGak&list=PLDdcY4olLQk066ysRibhoGd3UzGr0XSQG&index=5
 ** Time Complexity : O(V+E)
 ** Space Complexity :O(V), V size visited array
@@ -15,20 +19,22 @@ class Solution
     
     public boolean isCycleUtil(int V, int parent, ArrayList<ArrayList<Integer>> adj, boolean visited[]){
         
-        visited[V] = true;
-        ArrayList<Integer> neighbours = adj.get(V);
-        for(Integer neighbour : neighbours){
-            if(!visited[neighbour]){
-                boolean result = isCycleUtil(neighbour, V, adj, visited);
-                if(result){
+        visited[current] = true;
+
+        for (Integer neighbour : adj.get(current)) {
+            if (neighbour != parent) {
+                if (visited[neighbour]) {
                     return true;
+                } else {
+                    if (isCyclic(adj, current, neighbour, visited)) {
+                        return true;
+                    }
                 }
-            }else if(parent != neighbour){
-                return true;
             }
         }
-        
+
         return false;
+
     }
     
     //Function to detect cycle in an undirected graph.
