29b47e0166e30cec04893a011e7125a495dfea24
Vishal Singh <31471251+vsingh16@users.noreply.github.com>
2024-10-13 12:36:29 +0100

    Update StronglyConnectedComponent(kosarajus).java

--- src/graph/StronglyConnectedComponent(kosarajus).java
+++ src/graph/StronglyConnectedComponent(kosarajus).java
@@ -1,6 +1,6 @@
 /**
 ** https://www.geeksforgeeks.org/strongly-connected-components/
-** https://www.youtube.com/watch?v=RwkNLN5mBn8&list=PLDdcY4olLQk066ysRibhoGd3UzGr0XSQG&index=19
+** https://www.youtube.com/watch?v=59fUtYYz7ZU&t=155s. Apna College
 ** Note: To find components in undirected graph, we can simply apply DFS. Count when DFS is applied.
 ** For directed , we will apply Kosaraju's Algorithm
 ** A directed graph is strongly connected if there is a path between all pairs of vertices. 
@@ -11,81 +11,70 @@
 ** 2----            4
 SC = 3
 ** 
-** Approach : Kosaraju's Algorithm
-** 1. Create reverse of given graph
-** 2. Maintain order[] 
-** 3. Apply DFS in given graph and fill order[]. Fill it when a node is completely processed.
-** 4. Refer order[] in reverse order and apply DFS on reverse graph for node from order[].
-** 5. keep counter when DFS is invoked.
+** Approach: Kosaraju's Algorithm
+** Intuition: If we apply topological sorting, we can get U -> V in the graph.
+** Transpose of a graph tells if U -> V, then V -> U
+** 1. Topological Sort
+** 2. Graph Transpose
+** 3. Start from Topological Nodes and see how much we can travel
+** 4. keep counter when DFS is invoked.
 **/
 
-class Solution
-{
-    
-    
-    private void DFS(int node, ArrayList<ArrayList<Integer>> adj,
-    boolean visited[],ArrayList<Integer> order){
-        
-        visited[node] = true;
-        for(Integer neighbour : adj.get(node)){
-            if(!visited[neighbour]){
-                DFS(neighbour, adj, visited, order);
+public class Solution {
+
+    private Stack<Integer> stack;
+
+    public void dfs(int current, boolean visited[], ArrayList<ArrayList<Integer>> adj) {
+
+        visited[current] = true;
+        for (Integer neighbour : adj.get(current)) {
+            if (!visited[neighbour]) {
+                dfs(neighbour, visited, adj);
             }
         }
-        order.add(node);
+        stack.push(current);
     }
-    
-    private void DFSForReverse(int node, ArrayList<ArrayList<Integer>> adj,
-    boolean visited[]){
-        
-        visited[node] = true;
-        for(Integer neighbour : adj.get(node)){
-            if(!visited[neighbour]){
-                DFSForReverse(neighbour, adj, visited);
-            }
-        }
-    }
-    
-    
+
+
     //Function to find number of strongly connected components in the graph.
     // Time Complexity :O(V+E)
-    public int kosaraju(int V, ArrayList<ArrayList<Integer>> adj)
-    {
-        //Create Reverse Graph
-        ArrayList<ArrayList<Integer>> reverseAdj = new ArrayList<ArrayList<Integer>>();
-        for(int i=0;i<V;i++){
-            reverseAdj.add(new ArrayList<Integer>());
+    public int kosaraju(int V, ArrayList<ArrayList<Integer>> adj) {
+
+        //Topological Sort
+        boolean visited[] = new boolean[V];
+        stack = new Stack<>();
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                dfs(i, visited, adj);
+            }
         }
-        
-        
-        for(int i=0;i<V;i++){
-            for(Integer neighbour :adj.get(i)){
+
+        //Graph Transpose
+        ArrayList<ArrayList<Integer>> reverseAdj = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            reverseAdj.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < V; i++) {
+            for (Integer neighbour : adj.get(i)) {
                 reverseAdj.get(neighbour).add(i);
             }
         }
-        
-        // Apply DFS on original graph & 
-        // populate order array when we completely processed a node
-        ArrayList<Integer> order = new ArrayList();
-        boolean visited[] = new boolean[V];
-        for(int i=0;i<V;i++){
-            if(!visited[i]){
-                DFS(i, adj, visited,order);
+
+        //Start from Topological Nodes and see how much we can travel
+        visited = new boolean[V];
+        int count = 0;
+        while (!stack.isEmpty()) {
+            int current = stack.pop();
+            if (!visited[current]) {
+                dfs(current, visited, reverseAdj);
+                count++;
             }
         }
-        
-        
-        //Refer order list from back and invoke DFS and maintain counter
-        int counter = 0;
-        boolean visitedForReverse[] = new boolean[V];
-        for(int i = order.size()-1;i>=0;i--){
-            int node = order.get(i);
-            if(!visitedForReverse[node]){
-                DFSForReverse(node, reverseAdj, visitedForReverse);
-                counter++;
-            }
-        }
-        
-        return counter;
+
+        return count;
+
     }
+
 }
+
