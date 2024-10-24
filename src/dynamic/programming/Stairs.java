17d10828657c648f0d9a5f8b71a06aa1dd3ef5a3
Vishal Singh <31471251+vsingh16@users.noreply.github.com>
2024-10-24 16:11:56 +0100

    Update Stairs.java

--- src/dynamic/programming/Stairs.java
+++ src/dynamic/programming/Stairs.java
@@ -5,6 +5,11 @@ package dynamic.programming;
  * The person can climb either 1 stair or 2 stairs at a time. Count the number of ways, the person can reach the top.
  ** https://www.geeksforgeeks.org/count-ways-reach-nth-stair/
  ** https://www.techiedelight.com/find-total-ways-reach-nth-stair-with-atmost-m-steps/
+ ** https://www.youtube.com/watch?v=S31W3kohFDk&list=PLDzeHZWIZsTomOPnCiU3J95WufjE36wsb&index=2&t=397s
+ ** Approach: Any DP can be solved with below
+ ** Top Down: Recursion + Memorisation
+ ** Bottom Up: Tabular
+ ** Space Optimized
  */
 public class Stairs {
     
@@ -24,57 +29,98 @@ public class Stairs {
        }
     }
 
-    /**
-    ** Same above approach is applied in DP.
-    ** Time Complexity : O(n)
-    ** Space Complexity : O(n)
-    **/
-    int countWays(int n)
-    {
-        
-       int dp[] = new int[n+1];
-       for(int i=1;i<=n;i++){
-           if(i<=2){
-               dp[i] = i;
-           }else{
-               dp[i] = (dp[i-1]+dp[i-2])%1000000007;
-           }
-       }
-       
-       return dp[n];
-    }
-    
-    /**
-     * Number of ways to reach n stairs in m ways
-     * ways(n, m) = ways(n-1, m) + ways(n-2, m) + ... ways(n-m, m)(n<=m)
-     */
-    //Time Complexity:Exponential
-    public static int climbStairs(int n, int m) {
+    public static int nthStair(int n) {
 
-        if (n <= 1)
-            return n;
-        int res = 0;
-        for (int i = 1; i <= m && i <= n; i++)
-            res += climbStairs(n - i, m);
-        return res;
-    }
-
-    //Time Complexity:O(mn)
-    //Space Complexity:O(n)
-    // https://www.techiedelight.com/find-total-ways-reach-nth-stair-with-atmost-m-steps/
-    //Below implementation is asked when m>=2
-    public static int climbStairsDyn(int n, int m) {
-
-        int res[] = new int[n+1];
-        res[0] = 1;
-        res[1] = 1;
-        res[2] = 2;
-        for (int i = 3; i <= n; i++) {
-            res[i] = 0;
-            for (int j = 1; j <= m && j <= i; j++)
-                res[i] += res[i - j];
+        int dp[] = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i] = -1;
         }
-        return res[n - 1];
+
+        return nthStairBottomup(n, dp);
+    }
+
+    public static int nthStair(int n) {
+
+        int dp[] = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i] = -1;
+        }
+
+        return nthStairBottomup(n, dp);
+    }
+
+    /**
+    ** Recursion + Memorization
+    ** Time: O(n)
+    ** Sapce: O(1)
+    **/
+    public static int nthStairBottomup(int n, int dp[]) {
+
+        if (n <= 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 2;
+        }
+        if (dp[n] != -1) { // result already calculated
+            return dp[n];
+        }
+        dp[n] = dp[n-1]+dp[n-2];
+        return dp[n];
+    }
+
+
+    /**
+    ** Time: O(n)
+    ** Space: O(n)
+    **/
+   public static int nthStairTopDown(int n) {
+
+        if (n <= 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 2;
+        }
+        int dp[] = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+        }
+
+        return dp[n];
+    }
+
+    /**
+    ** Time : O(n)
+    ** Space: O(1)
+    **/
+    public static int nthStairTopDownSpaceOptimized(int n) {
+
+        if (n <= 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 2;
+        }
+
+        int prev1 = 2;
+        int prev2 = 1;
+        int current = 0;
+        for (int i = 3; i <= n; i++) {
+            current = (prev1 + prev2) % 1000000007;
+            prev2 = prev1;
+            prev1 = current;
+
+        }
+
+        return current;
     }
 
     public static void main(String[] args) {
