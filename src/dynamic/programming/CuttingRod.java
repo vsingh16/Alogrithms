fcb4ab8b32794891ead27f6eeac2b9740b356519
Vishal Singh <31471251+vsingh16@users.noreply.github.com>
2024-10-29 08:08:59 +0000

    Update CuttingRod.java

--- src/dynamic/programming/CuttingRod.java
+++ src/dynamic/programming/CuttingRod.java
@@ -1,59 +1,110 @@
 package dynamic.programming;
 
 /**
- * Created by vishal on 17-Jun-18.
- * <p>
- * Given a rod of length n inches and an array of prices that contains
- * prices of all pieces of size smaller than n. Determine the maximum value
- * obtainable by cutting up the rod and selling the pieces. For example,
- * if length of the rod is 8 and the values of different pieces are given as following,
- * then the maximum obtainable value is 22 (by cutting in two pieces of lengths 2 and 6)
- * https://www.geeksforgeeks.org/cutting-a-rod-dp-13/
- */
-public class CuttingRod {
+** https://www.geeksforgeeks.org/maximize-the-number-of-segments-of-length-p-q-and-r/
+**
+Given a rod of length L, the task is to cut the rod in such a way that the total number of segments of length p, q, and r is maximized. The segments can only be of length p, q, and r. 
+
+Examples: 
+
+Input: l = 11, p = 2, q = 3, r = 5 
+Output: 5 
+Explanation: Segments of 2, 2, 2, 2 and 3
 
 
-    /**
-     * At each l, we need to make l choices
-     * Time Complexity:O(l^l)
-     *
-     */
-    public static int maxProfit(int price[], int l) {
 
-        if (l <= 0) {
+Input: l = 7, p = 2, q = 5, r = 5 
+Output: 2 
+Explanation: Segments of 2 and 5
+** Ref: https://www.youtube.com/watch?v=MFAAZW2znv8&list=PLDzeHZWIZsTomOPnCiU3J95WufjE36wsb&index=6&t=150s
+** Approach: We can cut either x, y or z length and ans will be max of all of these.
+**/
+=================================================
+Recursion
+/**
+** Time Complexity: O(3^n)
+** Space Complexity: O(n)
+**/
+class Solution
+{
+    //Function to find the maximum number of cuts.
+    public int maximizeCuts(int n, int x, int y, int z)
+    {
+         if (n == 0) {
             return 0;
+        } else if (n < 0) {
+            return Integer.MIN_VALUE;
         }
 
-        //compare price of cutting at each length
-        Integer max = Integer.MIN_VALUE;
-        for (int i = 0; i <l; i++) {
-            max = Math.max(max, price[i] + maxProfit(price, l- 1- i));
-        }
-
-        return max;
-    }
-
-    /**
-     * Time Complexity : O(l *l)
-     * Space Complexity : O(l)
-     */
-    public static int maxProfitDyn(int price[], int l) {
-
-        int result[] = new int[l + 1];
-        for (int i = 1; i <= l; i++) {
-            Integer max = Integer.MIN_VALUE;
-            for (int j = 0; j < i; j++) {
-                max = Math.max(max, price[j] + result[i - j - 1]);
-            }
-            result[i] = max;
-        }
-
-        return result[l];
-    }
-
-    public static void main(String[] args) {
-        int price[] = {1, 5, 8, 9, 10, 17, 17, 20};
-        System.out.println(maxProfit(price, price.length));
-        System.out.println(maxProfitDyn(price, price.length));
+        int a = 1 + maximizeCuts(n - x, x, y, z);
+        int b = 1 + maximizeCuts(n - y, x, y, z);
+        int c = 1 + maximizeCuts(n - z, x, y, z);
+        return Math.max(Math.max(a, b), c);
     }
 }
+=====================================================
+Recusion + Memorisation
+
+/**
+** Time Complexity: O(n)
+** Space Complexity: O(n)
+**/
+class Solution
+{
+    //Function to find the maximum number of cuts.
+    public int maximizeCuts(int n, int x, int y, int z)
+    {
+        int results[] = new int[n + 1];
+        Arrays.fill(results, -1);
+        int result = maximizeCuts(n, x, y, z, results);
+        return result < 0 ? 0 : result; ////Return 0 if ans not possible
+    }
+    
+     public int maximizeCuts(int n, int x, int y, int z, int results[]) {
+        if (n == 0) {
+            return 0;
+        } else if (n < 0) {
+            return Integer.MIN_VALUE;
+        } else if (results[n] != -1) { // Already Calculated
+            return results[n];
+        }
+
+        int a = maximizeCuts(n - x, x, y, z, results);
+        int b = maximizeCuts(n - y, x, y, z, results);
+        int c = maximizeCuts(n - z, x, y, z, results);
+
+        a = (a == Integer.MIN_VALUE) ? Integer.MIN_VALUE : 1 + a; //Need to handle case when receiving -ve value as cant add 1 to it because -ve means already not possible.
+        b = (b == Integer.MIN_VALUE) ? Integer.MIN_VALUE : 1 + b;
+        c = (c == Integer.MIN_VALUE) ? Integer.MIN_VALUE : 1 + c;
+
+        results[n] = Math.max(Math.max(a, b), c);
+        return results[n];
+    }
+}
+=====================================================
+Top Down
+/**
+** Time Complexity: O(n)
+** Space Complexity: O(n)
+**/
+class Solution
+{
+    //Function to find the maximum number of cuts.
+    public int maximizeCuts(int n, int x, int y, int z)
+    {
+      int dp[] = new int[n + 1];
+        dp[0] = 0;
+        for (int i = 1; i < dp.length; i++) {
+            int a = ((i - x) >= 0 && dp[i - x] >= 0) ? 1 + dp[i - x] : Integer.MIN_VALUE; ////Need to handle case when receiving -ve value as cant add 1 to it because -ve means already not possible. ALso -ve array indexes
+            int b = ((i - y) >= 0 && dp[i - y] >= 0) ? 1 + dp[i - y] : Integer.MIN_VALUE;
+            int c = ((i - z) >= 0 && dp[i - z] >= 0) ? 1 + dp[i - z] : Integer.MIN_VALUE;
+            dp[i] = Math.max(Math.max(a, b), c);
+        }
+
+        return dp[n] == Integer.MIN_VALUE ? 0 : dp[n]; //Return 0 if ans not possible
+    }
+    
+  
+}
+
+
