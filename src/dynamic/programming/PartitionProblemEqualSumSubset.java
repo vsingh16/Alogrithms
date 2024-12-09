878b1f8987bcfb4b9281ab0779e85787bf9f3c96
Vishal Singh <31471251+vsingh16@users.noreply.github.com>
2024-12-09 11:32:23 +0000

    Update PartitionProblemEqualSumSubset.java

--- src/dynamic/programming/PartitionProblemEqualSumSubset.java
+++ src/dynamic/programming/PartitionProblemEqualSumSubset.java
@@ -1,5 +1,7 @@
 /**
 ** https://www.geeksforgeeks.org/partition-problem-dp-18/
+** https://leetcode.com/problems/partition-equal-subset-sum/submissions/1474276868/
+** https://www.youtube.com/watch?v=UGY7FMHt-M8&list=PLDzeHZWIZsTomOPnCiU3J95WufjE36wsb&index=23&t=81s. Love Babbar
 ** Partition problem is to determine whether a given set can be partitioned into two subsets such that the sum of elements in both subsets is the same. 
 
 Examples: 
@@ -16,75 +18,213 @@ The array cannot be partitioned into equal sum sets.
 ** else find if subset is possible for sum/2 in given array. : https://github.com/vsingh16/Alogrithms/blob/master/src/dynamic/programming/CountSubsetWithGivenSum.java
 **/
 
-class Solution{
-  
-  // Time Complexity :O(2^n)
-  boolean isSusbset(int n, int w){
-  //i.e target sum achieved
-  if(w == 0){
-    return true;
-  }
-  
-  //if sum not achieved and no items left
-  if(n == 0){
-    return false;
-  }
-  
-  //if item is gretart than target weight, we have to exclude it
-  if(arr[n-1] > w){
-    return isSusbset(n-1, w)
-  }else{                
-    return isSusbset(n-1, w)  || countSusbset(n-1, w - arr[n-1]);
-  }
+/**
+** Recursion(TOp Down Approach, as starting from Left 0th Index):
+** Time Complexity: O(2^n)
+** Space Complexity: O(n)
+**/
+class Solution {
+
+     static boolean isSusbset(int nums[], int index, int targetSum) {
+
+        if (targetSum == 0) {
+            return true;
+        } else if (index == nums.length) {
+            return false;
+        }
+
+
+        boolean include = false;
+        if (nums[index] <= targetSum) {
+            include = isSusbset(nums, index + 1, targetSum - nums[index]);
+        }
+
+        boolean exclude = isSusbset(nums, index + 1, targetSum);
+
+        return include || exclude;
+
+    }
+
+    public boolean canPartition(int[] nums) {
+           //Find Sum
+        int sum = Arrays.stream(nums).sum();
+        if (sum % 2 != 0) {
+            return false;
+        } else {
+            return isSusbset(nums, 0, sum / 2);
+        }
+    }
+}
+==========================================================
+/**
+** Recursion(Bottom Up) + Memorization
+** Time Complexity: O(n * sum/2)
+** Space Complexity: O(n * sum/2)
+**/
+class Solution {
+
+    static boolean isSusbsetMem(int nums[], int index, int targetSum, int dp[][]) {
+
+        if (targetSum == 0) {
+            return true;
+        } else if (index == nums.length) {
+            return false;
+        } else if (dp[index][targetSum] != -1) { //Already Calculated. 0 = false . 1 = true
+            return dp[index][targetSum] == 1 ? true : false;
+        }
+
+
+        boolean include = false;
+        if (nums[index] <= targetSum) {
+            include = isSusbsetMem(nums, index + 1, targetSum - nums[index], dp);
+        }
+
+        boolean exclude = isSusbsetMem(nums, index + 1, targetSum, dp);
+
+        boolean result = include || exclude;
+        dp[index][targetSum] = result == true ? 1 : 0;
+        return result;
+
+    }
+
+
+    public static boolean canPartition(int[] nums) {
+
+        //Find Sum
+        int sum = Arrays.stream(nums).sum();
+        int dp[][] = new int[nums.length][(sum / 2) + 1];
+        for (int i = 0; i < dp.length; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        if (sum % 2 != 0) {
+            return false;
+        } else {
+            return isSusbsetMem(nums, 0, sum / 2, dp);
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        int nums[] = {1, 2, 5};
+        System.out.println(canPartition(nums));
+    }
 }
 
-  // Time Complexity :O(2^n)
-  static boolean equalPartition(int n, int arr[]){
-    
-    int sum = 0;
-    for(int i=0;i<n;i++){
-      sum = sum + arr[i];
-    }
-    
-    //odd
-    if(sum %2 != 0){
-      return false;
-    }
-    
-    return IsSusbset(n, sum/2); // // Time Complexity :O(2^n)
-  }
-  
-  // Time Complexity : n * sum/2
-    static int equalPartition(int n, int arr[])
-    {
-        int sum =0;
-        for(int i=0;i<n;i++){
-            sum = sum + arr[i];
+==========================================================
+/**
+** Bottom Up Approach(Because Starting from last index). Tabluar Approach is just opposite of recursion.
+** This is opposite of recursion but we need to handle few scenarios.
+
+** Time Complexity: O(n*sum/2)
+** Space Complexity: O(n*sum/2) 
+**/
+
+class Solution {
+
+    static boolean isSusbset(int nums[], int targetSum) {
+
+        boolean dp[][] = new boolean[nums.length + 1][targetSum + 1]; // +1 to handle Array out of bound
+
+        //Base Case Handling
+        for (int i = 0; i < nums.length; i++) {
+            dp[i][0] = true;
         }
-        
-        //Odd, can't be equal partition, return false
-        if(sum%2!=0){
-            return 0;
-        }
-        
-        int halfSum = sum/2;
-        boolean dp[][] = new boolean[n+1][halfSum+1];
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=halfSum;j++){
-                if(i == 0 && j == 0){
-                    dp[i][j] = true;
-                }else if(i == 0 && j != 0){
-                    dp[i][j] = false;
-                }else if(i !=0 && j == 0){
-                    dp[i][j] = false;
-                }else if(arr[i-1] > j){
-                    dp[i][j] = dp[i-1][j]; //exclude
-                }else{
-                    dp[i][j] = dp[i-1][j] ||
-                    dp[i-1][j - arr[i-1]]; //exclude & include
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            for (int sum = targetSum; sum > 0; sum--) { //Since sum=0, already calculated
+
+                boolean include = false;
+                if (nums[i] <= sum) {
+                    include = dp[i + 1][sum - nums[i]];
                 }
+
+                boolean exclude = dp[i + 1][sum];
+                dp[i][sum] = include || exclude;
+
             }
         }
-        
-        return dp[n][halfSum] ? 1 : 0;
+
+        return dp[0][targetSum];
+
     }
+
+    public static boolean canPartition(int[] nums) {
+
+        //Find Sum
+        int sum = Arrays.stream(nums).sum();
+
+        if (sum % 2 != 0) {
+            return false;
+        } else {
+            return isSusbset(nums, sum / 2);
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        int nums[] = {1, 5, 11, 5};
+        System.out.println(canPartition(nums));
+    }
+}
+
+=================================================
+/**
+** Space Optimized DP
+** Time Complexity: O(n*sum/2)
+** Space Complexity: O(sum/2) 
+**/
+
+import java.util.Arrays;
+
+class Solution {
+
+    static boolean isSusbset(int nums[], int targetSum) {
+
+        boolean currentDp[] = new boolean[targetSum + 1]; // +1 to handle Array out of bound
+        boolean previousDp[] = new boolean[targetSum + 1]; // +1 to handle Array out of bound
+
+        //Base Case Handling
+        for (int i = 0; i < nums.length; i++) {
+            previousDp[0] = true;
+        }
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            for (int sum = targetSum; sum > 0; sum--) { //Since sum=0, already calculated
+
+                boolean include = false;
+                if (nums[i] <= sum) {
+                    include = previousDp[sum - nums[i]];
+                }
+
+                boolean exclude = previousDp[sum];
+                currentDp[sum] = include || exclude;
+
+            }
+            previousDp = Arrays.copyOf(currentDp, currentDp.length);
+        }
+
+        return previousDp[targetSum];
+
+    }
+
+    public static boolean canPartition(int[] nums) {
+
+        //Find Sum
+        int sum = Arrays.stream(nums).sum();
+
+        if (sum % 2 != 0) {
+            return false;
+        } else {
+            return isSusbset(nums, sum / 2);
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        int nums[] = {1, 5, 11, 5};
+        System.out.println(canPartition(nums));
+    }
+}
