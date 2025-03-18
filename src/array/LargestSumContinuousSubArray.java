f21cbe150ad0661f3d58bab2cb06e1f5e3938690
Vishal Singh <31471251+vsingh16@users.noreply.github.com>
2025-03-18 07:56:16 +0000

    Update LargestSumContinuousSubArray.java

--- src/array/LargestSumContinuousSubArray.java
+++ src/array/LargestSumContinuousSubArray.java
@@ -1,75 +1,73 @@
 package array;
 
 /**
- * Created by vishal on 09-Jul-18.
- * <p>
+ * Ref: https://www.youtube.com/watch?v=9IZYqostl2M&t=972s. Apna College
  * https://www.geeksforgeeks.org/largest-sum-contiguous-subarray/
- * <p>
- * If array has all positive numbers , we can simply add them.total sum will be max sum
- * <p>
- * But since array has -ve numbers also, so we will use Kadane Algorithm.
- * i.e max possible sum = 0 (+ve number)
- * <p>
- * so keep on adding elements , if at any point sum_so_far <0 , make it zero
+ * https://leetcode.com/problems/maximum-subarray/description/
+ * Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+ 
+
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+Example 2:
+
+Input: nums = [1]
+Output: 1
+Explanation: The subarray [1] has the largest sum 1.
+Example 3:
+
+Input: nums = [5,4,-1,7,8]
+Output: 23
+Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
  */
-public class LargestSumContinuousSubArray {
 
-    /**
-     * Approach: Check for all possible sub array
-     * Time Complexity : O(n^2)
-     */
-    public static int findMaxSum(int a[]) {
-
-        int maxSum = Integer.MIN_VALUE;
-        for (int i = 0; i < a.length; i++) {
-            int sum = a[i];
-            for (int j = i + 1; j < a.length; j++) {
-                sum = sum + a[j];
-                maxSum = Math.max(sum, maxSum);
+/**
+** Brute Force Approach: Explore all Possible Sub Array.
+** Every Sub Array has start and end index.
+** Time Complexity: O(n^3) 
+** Space Complexity: O(1)
+**/
+class Solution {
+    public int maxSubArray(int[] a) {
+     int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < a.length; i++) { //Start Index
+            for (int j = i; j < a.length; j++) { //End Index
+                int sum = 0;
+                for (int k = i; k <= j; k++) { //Sum of Sub Array
+                    sum = sum + a[k];
+                    maxSum = Math.max(sum, maxSum);
+                }
             }
         }
 
         return maxSum;
     }
+}
+==========================================================
+/**
+** Kadane's Algorithm.
+** Sum all elements. If any point sum is <=0, reset sum=0 i.e discard the sub array giving us 0
+** Below code can also handle case when all elements in array is -ve. As maxSum is only updated if sum is > maxSum
+** Time Complexity: O(n) 
+** Space Complexity: O(1)
+**/
 
-    //Time Complexity:O(n)
-    public static int findMaxSumKadane(int a[]) {
-
-        int sum = 0, maxSum = Integer.MIN_VALUE;
+class Solution {
+    public int maxSubArray(int[] a) {
+   int maxSum = Integer.MIN_VALUE;
+        int sum = 0;
         for (int i = 0; i < a.length; i++) {
-            sum = sum + a[i];
-            if (sum < 0) {
-                sum = 0;
+            sum += a[i];
+            maxSum = Math.max(maxSum, sum);
+            if (sum <= 0) {
+                sum = 0; //Discarding this sub array
             }
-            maxSum = Math.max(sum, maxSum);
         }
 
         return maxSum;
-    }
-
-    /**
-     * Case where all array elements are -ve,
-     * SO we cant say if sum<0, sum = 0
-     * but we will do sum = Math.max(sum+a[i],a[i])
-     *
-     */
-    public static int findMaxSumWithOnlyNegativeNumbers(int a[]) {
-
-        int sum = 0, maxSum = Integer.MIN_VALUE;
-        for (int i = 0; i < a.length; i++) {
-            sum = Math.max(a[i], sum + a[i]);
-            maxSum = Math.max(sum, maxSum);
-        }
-
-        return maxSum;
-    }
-
-    public static void main(String[] args) {
-        int a[] = {-2, -3, 4, -1, -2, 1, 5, -3};
-        System.out.println(findMaxSum(a));
-        System.out.println(findMaxSumKadane(a));
-
-        int a1[] = {-2, -3, -4, -1, -2, -1, -5, -3};
-        System.out.println(findMaxSumWithOnlyNegativeNumbers(a1));
     }
 }
