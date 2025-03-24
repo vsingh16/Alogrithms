a6b8eff27e25d4dbb636dcbbabc650bcee5e3ddd
Vishal Singh <31471251+vsingh16@users.noreply.github.com>
2025-03-24 07:38:26 +0000

    Update ProductArrayPuzzle.java

--- src/array/ProductArrayPuzzle.java
+++ src/array/ProductArrayPuzzle.java
@@ -4,6 +4,8 @@ package com.macquarie.shiner.batch.gcs.tasklet;
  * @author vsingh16
  *         <p>
  *         https://www.geeksforgeeks.org/a-product-array-puzzle/
+ * https://leetcode.com/problems/product-of-array-except-self/description/
+ * https://www.youtube.com/watch?v=TW2m8m_FNJE&list=PLfqMhTWNBTe137I_EPQd34TsgV6IO55pt&t=1293s. Apna College
  * Given an array arr[] of n integers, construct a Product Array prod[] (of the same size) such that prod[i] is equal to the product of all the elements of arr[] except arr[i]. 
 
 Note: Solve it without the division operator in O(n) time.
@@ -27,7 +29,12 @@ elements except 2 is 900
 ** a[i] = left[i] * right[I]
 ** Take 2 arrays, left and right.
 ** Populate left and right.
+** Approach 3: We can reduce the extra array
+** First Store Prefixes in the result array.
+** Secondly calculate and store suffixes in the same array.
  */
+
+Approach 2:
 public class ProductArrayPuzzle {
 
     public static void main(String[] args) {
@@ -49,5 +56,27 @@ public class ProductArrayPuzzle {
         for (int i = 0; i < a.length; i++) {
             System.out.println(left[i] * right[i] + " ");
         }
+    }
+}
+========================================================================
+Approach 3: We can reduce the extra array    
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+          int result[] = new int[nums.length];
+
+        //Prefix
+        result[0] = 1;
+        for (int i = 1; i < result.length; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+
+        //Suffix
+        int suffix = 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            suffix = suffix * nums[i + 1];
+            result[i] = result[i] * suffix;
+        }
+
+        return result;
     }
 }
