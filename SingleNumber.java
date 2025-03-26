a58ddcaf9076ef03949349e0c38e3414828b2cdd
Vishal Singh <31471251+vsingh16@users.noreply.github.com>
2025-03-26 07:21:48 +0000

    Update SingleNumber.java

--- SingleNumber.java
+++ SingleNumber.java
@@ -6,19 +6,14 @@ You are given a sorted array consisting of only integers where every element app
 Return the single element that appears only once.
 
 Your solution must run in O(log n) time and O(1) space.
+Example 1:
 
- 
-*
-* 
-*
-* Example 1:
-*
-* Input: nums = [2,2,1]
-* Output: 1
-* Example 2:
-*
-* Input: nums = [4,1,2,1,2]
-* Output: 4
+Input: nums = [1,1,2,3,3,4,4,8,8]
+Output: 2
+Example 2:
+
+Input: nums = [3,3,7,7,10,11,11]
+Output: 10
 * Approach 1: Traverse and check if (a[i - 1] != a[i] && a[mid] != a[i + 1]), return a[I]
 ** Approach 2: Since Array is sorted we can use Binary Search
 **/
