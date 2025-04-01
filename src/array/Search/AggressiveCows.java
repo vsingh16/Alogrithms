ba586101bd5e3c79ea6d87840a81c999ea4f83df
Vishal Singh <31471251+vsingh16@users.noreply.github.com>
2025-04-01 15:46:15 +0100

    Update AggressiveCows.java

--- src/array/Search/AggressiveCows.java
+++ src/array/Search/AggressiveCows.java
@@ -1,5 +1,7 @@
 /**
 ** https://www.spoj.com/problems/AGGRCOW/
+** https://www.youtube.com/watch?v=7wOzDqsfXy0 . Apna College
+** https://www.youtube.com/watch?v=YTTdLgyqOLY&t=3394s
 ** Farmer John has built a new long barn, with N (2 <= N <= 100,000) stalls. The stalls are located along a straight line at positions x1 ... xN (0 <= xi <= 1,000,000,000).
 
 ** His C (2 <= C <= N) cows don't like this barn layout and become aggressive towards each other once put into a stall. To prevent the cows from hurting each other, FJ wants to assign the cows to the stalls, such that the minimum distance between any two of them is as large as possible. What is the largest minimum distance?
@@ -27,11 +29,10 @@
 ** FJ can put his 3 cows in the stalls at positions 1, 4 and 8,
 ** resulting in a minimum distance of 3.
 ** Approach:
-** Ref: https://www.youtube.com/watch?v=YTTdLgyqOLY
 ** The largest minimum distance. This gives a hint on solving it using search space.
 ** Since we can ignore one part as if a cow is fighting at x distance, they will fight at all x-1 distances, so we can ignore it.
 ** We can apply binary search.
-** Time Complexity: O(nlogn), as we need to sort first
+** Time Complexity: Max of (O(nlogn), O(n * log search space))
 ** Space Complexity: O(1)
 **/
 
@@ -41,12 +42,12 @@ class Solution {
   ** If can be allocated, assign and increase count.
   ** Last Pos also get updated as now Cow is at this place.
   **/
-    public static boolean isPossibleSol(int stalls[], int k, int mid) {
+    public static boolean isPossibleSol(int stalls[], int k, int mid) { 
 
         int cowCount = 1;
         int lastPos = stalls[0];
-        for (int i = 1; i < stalls.length; i++) {
-            if (Math.abs(stalls[i] - lastPos) >= mid) {
+        for (int i = 1; i < stalls.length; i++) { //o(log stalls length)
+            if (stalls[i] - posOfLastCow >= mid) {
                 cowCount++;
                 lastPos = stalls[i];
                 if (cowCount == k) {
@@ -58,12 +59,14 @@ class Solution {
         return false;
     }
 
+    
     public static int aggressiveCows(int stalls[], int k) {
-        Arrays.sort(stalls);
-        int l = 0;
-        int h = Arrays.stream(stalls).max().getAsInt();
+        Arrays.sort(stalls); //nlogn
+        int l = 0; //We can use 1 min possible distance or 0 will also work
+        int h = Arrays.stream(stalls).max().getAsInt(); // we can use Arrays.stream(stalls).max().getAsInt() or to be precise
+        // int h = Arrays.stream(stalls).max().getAsInt() - Arrays.stream(stalls).min().getAsInt();
         int ans = -1;
-        while (l <= h) {
+        while (l <= h) { //o(log search space)
             int mid = l + (h - l) / 2;
             if (isPossibleSol(stalls, k, mid)) {
                 ans = mid;
