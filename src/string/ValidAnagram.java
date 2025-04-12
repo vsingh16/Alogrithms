3acd606512da7daa74cd4d3358ae179e1a18f236
Vishal Singh <31471251+vsingh16@users.noreply.github.com>
2025-04-12 08:33:14 +0100

    Update ValidAnagram.java

--- src/string/ValidAnagram.java
+++ src/string/ValidAnagram.java
@@ -1,6 +1,7 @@
 /**
 ** Ref: https://leetcode.com/problems/valid-anagram/description/
 Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+An anagram is a word or phrase formed by rearranging the letters of a different word or phrase, using all the original letters exactly once.
 
 Example 1:
 
