f467a21b49afd98d5a65cf774a4ac08825ddfc26
Vishal Singh <31471251+vsingh16@users.noreply.github.com>
2025-06-15 08:30:59 +0100

    Update BionomialCoefficient.java

--- src/dynamic/programming/BionomialCoefficient.java
+++ src/dynamic/programming/BionomialCoefficient.java
@@ -1,13 +1,14 @@
 package dynamic.programming;
 
 /**
+* Ref: https://www.geeksforgeeks.org/dsa/permutation-coefficient/
  * Created by vishal on 06-Mar-18.
  * <p>
  * C(n,k) = n!/(n-k)!*k!
  * <p>
  * Number of ways we can choose subset of size k among n elements
  * <p>
- * Method 1 : Calculate the values as per formulae.
+ * Method 1 : Calculate the values as per the formulae.
  * <p>
  * Method 2:Recursion  C(n, k) = C(n-1, k-1) + C(n-1, k) with Base case: C(n, 0) = C(n, n) = 1, Exponential Time complexity
  * <p>
@@ -19,64 +20,141 @@ package dynamic.programming;
  * Time Complexity:O(k)
  * Space Complexity:O(1)
  */
-public class BionomialCoefficient {
-
-    public static int binomialCoeff(int n, int k) {
-        int res = 1;
-
-        // Since C(n, k) = C(n, n-k)//eg C(5,2) = C(5,3), so if K < (n-k) use K else (n-k)
-        if (k > n - k)
-            k = n - k;
-
-        // Calculate value of [n * (n-1) *---* (n-k+1)] / [k * (k-1) *----* 1]
-        for (int i = 0; i < k; i++) {
-            res = res * (n - i);
-            res = res / (i + 1);
-        }
-
-        return res;
-    }
-
-    public static int binomialCoeffRec(int n, int k) {
-
-        if (k == 0) {
+/**
+** Bottm Up(Recursion)
+** Time Complexity: O(2^n)
+** Space Complexity: O(n)
+**/
+class Solution {
+    public int nCr(int n, int r) {
+          //Base Case
+        if (r == 0) {
             return 1;
-        } else if (k == n) {
+        } else if (r == n) {
             return 1;
+        } else if (r > n) {
+            return 0;
         }
 
-        if (n - k < k) {
-            k = n - k;
+
+        int include = nCr(n - 1, r - 1);
+        int exclude = nCr(n - 1, r);
+
+        return include + exclude;
+        
+    }
+}
+===================================================================
+/**
+** Bottm Up(Recursion) + Memorization
+** Time Complexity: O(n1*r1)
+** Space Complexity: O(n1*r1)
+**/
+
+class Solution {
+    
+    public int nCrUtil(int n, int r, int dp[][]) {
+
+        //Base Case
+        if (r == 0) {
+            return 1;
+        } else if (r == n) {
+            return 1;
+        } else if (r > n) {
+            return 0;
+        } else if (dp[n][r] != -1) { //Already Calculated
+            return dp[n][r];
         }
 
-        return binomialCoeffRec(n - 1, k - 1) + binomialCoeffRec(n - 1, k);
+        int include = nCrUtil(n - 1, r - 1, dp);
+        int exclude = nCrUtil(n - 1, r, dp);
+
+        dp[n][r] = include + exclude;
+        return dp[n][r];
 
     }
+    
+    
+    public int nCr(int n, int r) {
+     int dp[][] = new int[n + 1][r + 1];
+        for (int i = 0; i < dp.length; i++) {
+            Arrays.fill(dp[i], -1);
+        }
 
-    public static int binomialCoeffDyn(int n, int k) {
+        return nCrUtil(n, r, dp);
+        
+    }
+}
 
-        int res[][] = new int[n + 1][k + 1];
+============================================
+/**
+** Top Down DP
+** Time Complexity: O(n1*r1)
+** Space Complexity: O(n1*r1)
+**/
 
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= Integer.min(i, k); j++) {
+class Solution {
+    
+    public int nCr(int n1, int r1) {
+    int dp[][] = new int[n1 + 1][r1 + 1];
 
-                if (j == 0 || i == j) {
-                    res[i][j] = 1;
+        for (int n = 0; n <= n1; n++) {
+            for (int r = 0; r <= r1; r++) {
+                //Base Case
+                if (r == 0) {
+                    dp[n][r] = 1;
+                } else if (r == n) {
+                    dp[n][r] = 1;
+                } else if (r > n) {
+                    dp[n][r] = 0;
                 } else {
-                    res[i][j] = res[i - 1][j - 1] + res[i - 1][j];
+                    int include = dp[n - 1][r - 1];
+                    int exclude = dp[n - 1][r];
+
+                    dp[n][r] = include + exclude;
                 }
             }
         }
 
-        return res[n][k];
-
-    }
-
-    public static void main(String[] args) {
-
-        System.out.println(binomialCoeff(5, 2)); //output :10
-        System.out.println(binomialCoeffRec(5, 2));
-        System.out.println(binomialCoeffDyn(5, 2));
-
+        return dp[n1][r1];
+        
     }
 }
+========================================================
+/**
+** Top Down DP + Space Optimized
+** Time Complexity: O(n1*r1)
+** Space Complexity: O(r1)
+**/
+
+class Solution {
+    
+    public int nCr(int n1, int r1) {
+        int previousDp[] = new int[r1 + 1]; //This array will be of internal loop
+        int currentDp[] = new int[r1 + 1];
+
+        for (int n = 0; n <= n1; n++) {
+            for (int r = 0; r <= r1; r++) {
+                //Base Case
+                if (r == 0) {
+                    currentDp[r] = 1;
+                } else if (r == n) {
+                    currentDp[r] = 1;
+                } else if (r > n) {
+                    currentDp[r] = 0;
+                } else {
+                    int include = previousDp[r - 1];
+                    int exclude = previousDp[r];
+
+                    currentDp[r] = include + exclude;
+                }
+            }
+
+            //Copy current to previous
+            previousDp = Arrays.copyOf(currentDp, currentDp.length);
+        }
+
+        return previousDp[r1];
+    }
+}
+
