26e1289d722c1e4e2792e65694fa5f6b42219be2
Vishal Singh <31471251+vsingh16@users.noreply.github.com>
2025-07-03 07:24:14 +0100

    Update DFSTraversal.java

--- src/graph/DFSTraversal.java
+++ src/graph/DFSTraversal.java
@@ -10,70 +10,41 @@
 ** Space complexity: O(V) v stack frame calls due to recursion and since we are only doing fun call on not visited nodes
 **/
 
-// { Driver Code Starts
-// Initial Template for Java
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-class GFG
-{
-    public static void main(String[] args) throws IOException
-    {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-        while(T-->0)
-        {
-            String[] s = br.readLine().trim().split(" ");
-            int V = Integer.parseInt(s[0]);
-            int E = Integer.parseInt(s[1]);
-            ArrayList<ArrayList<Integer>>adj = new ArrayList<ArrayList<Integer>>();
-            for(int i = 0; i < V; i++)
-                adj.add(new ArrayList<Integer>());
-            for(int i = 0; i < E; i++){
-                String[] S = br.readLine().trim().split(" ");
-                int u = Integer.parseInt(S[0]);
-                int v = Integer.parseInt(S[1]);
-                adj.get(u).add(v);
-                adj.get(v).add(u);
-            }
-            Solution obj = new Solution();
-            ArrayList<Integer>ans = obj.dfsOfGraph(V, adj);
-            for (int i =0 ;i < ans.size (); i++) 
-                System.out.print (ans.get (i) + " ");
-            System.out.println();
-        }
-    }
-}
-// } Driver Code Ends
-
-
-
-
-
-class Solution
-{
+class Solution {
     
     ArrayList<Integer> result;
     
-    //Function to return a list containing the DFS traversal of the graph.
-    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj)
-    {
-        result = new ArrayList();
-        boolean visited[] = new boolean[V];
-        dfsUtil(0, visited, adj);
-        
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> adj) {
+    ArrayList<Integer> result = new ArrayList<>();
+        int v = adj.size();
+        boolean visited[] = new boolean[v];
+        /**
+        ** If a graph is connected, we can start from the first node i.e 0
+        ** Since disconnected, we need to do a traversal from each node
+        **/
+        for (int i = 0; i < v; i++) { 
+            if (!visited[i]) {//Before fun call
+                visited[i] = true; //Marking of visisted has to be in sync as in recursive fun call
+                dfs(i, visited, result, adj);
+            }
+        }
+
         return result;
     }
     
-    private void dfsUtil(int V, boolean visited[], ArrayList<ArrayList<Integer>> adj){
-        
-        result.add(V);
-        visited[V] = true;
-        ArrayList<Integer> neighbours = adj.get(V);
-        for(Integer neighbour:neighbours){
-            if(!visited[neighbour]){
-              dfsUtil(neighbour, visited,adj);
+    public ArrayList<Integer> dfs(int vertice, boolean visited[], ArrayList<Integer> result, ArrayList<ArrayList<Integer>> adj) {
+
+    result.add(vertice);
+
+        ArrayList<Integer> neighbours = adj.get(vertice);
+        for (Integer neighbour : neighbours) {
+            if (!visited[neighbour]) { //Since here I am checking and after that marking node visisted, same we have to do for 1st node i.e from main fun.
+                visited[neighbour] = true;
+                dfs(neighbour, visited, result, adj);
             }
         }
+
+        return result;
     }
 }
