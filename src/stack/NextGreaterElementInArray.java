dcb23b28d39e4913cb031a149d1783d386eed675
Vishal Singh <31471251+vsingh16@users.noreply.github.com>
2025-07-26 09:13:12 +0100

    Update NextGreaterElementInArray.java

--- src/stack/NextGreaterElementInArray.java
+++ src/stack/NextGreaterElementInArray.java
@@ -32,28 +32,6 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class GFG {
-    
-	public static void main (String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int t = Integer.parseInt(br.readLine().trim());
-		while(t-->0){
-		    int n = Integer.parseInt(br.readLine().trim());
-		    String inputLine[] = br.readLine().trim().split(" ");
-		    long[] arr = new long[n];
-		    for(int i=0; i<n; i++)arr[i]=Long.parseLong(inputLine[i]);
-		    long[] res = new solve().nextLargerElement(arr, n);
-		    for (int i = 0; i < n; i++) 
-		        System.out.print(res[i] + " ");
-		    System.out.println();
-		}
-	}
-}
-
-
-
-// } Driver Code Ends
-
 
 class solve{
     public static long[] nextLargerElement(long[] arr, int n) { 
